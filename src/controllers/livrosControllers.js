710a6d404fbcb6afa76b1579fe563dc314ea8f72
Lucas Mendes <lucsmends09@gmail.com>
2023-02-23 13:27:54 -0300

    aula-4 separando responsabilidades

---
+++ src/controllers/livrosControllers.js
@@ -0,0 +1,59 @@
+import livros from "../models/Livro.js";
+
+class LivroController{
+    static listarLivros = (req, res) =>{
+        livros.find((err, livros) => {
+            res.status(200).json(livros)
+        })
+    }
+
+    static listarLivrosPorId = (req, res) =>{
+        const id = req.params.id;
+
+        livros.findById(id, (err, livros)=>{
+            if (err){
+                res.status(400).send({message: `${err.message} - Livro não localizado.`})
+            }else{
+                res.status(200).send(livros)
+            }
+        })
+    }
+
+    static cadastrarLivro = (req, res) =>{
+        let livro = new livros(req.body)
+
+        livro.save((err) => {
+            if(err){
+                res.status(500).send({message: `${err.message} - falha ao cadastrar`})
+            } else {
+                res.status(201).send(livro.toJSON())
+            }
+        })
+    }
+
+    static atualizarLivro = (req, res) =>{
+        const id = req.params.id
+
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err)=>{
+            if(!err){
+                res.status(200).send({message: 'livro atualizado com sucesso'})
+            } else {
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
+
+    static deletarLivro = (req, res) =>{
+        const id = req.params.id
+
+        livros.findByIdAndDelete(id, (err)=>{
+            if(!err){
+                res.status(200).send({message: 'Livro removido com sucesso'})
+            } else {
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
+}
+
+export default LivroController
