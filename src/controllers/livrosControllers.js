19919d3b8d7260f69098a56c645115e300702a11
Lucas Mendes <lucsmends09@gmail.com>
2023-02-23 14:13:39 -0300

    aula-5 terminando a api

--- src/controllers/livrosControllers.js
+++ src/controllers/livrosControllers.js
@@ -2,7 +2,9 @@ import livros from "../models/Livro.js";
 
 class LivroController{
     static listarLivros = (req, res) =>{
-        livros.find((err, livros) => {
+        livros.find()
+        .populate('autor')
+        .exec((err, livros) => {
             res.status(200).json(livros)
         })
     }
@@ -10,7 +12,10 @@ class LivroController{
     static listarLivrosPorId = (req, res) =>{
         const id = req.params.id;
 
-        livros.findById(id, (err, livros)=>{
+        livros.findById(id)
+        .populate('autor', 'name')
+
+        .exec((err, livros) => { 
             if (err){
                 res.status(400).send({message: `${err.message} - Livro não localizado.`})
             }else{
@@ -54,6 +59,14 @@ class LivroController{
             }
         })
     }
+
+    static listarLivrosPorEditora =(req, res) =>{
+        const editora = req.query.editora
+
+        livros.find({'editora': editora}, {}, (err, livros) =>{
+            res.status(200).send(livros)
+        })
+    } 
 }
 
 export default LivroController
